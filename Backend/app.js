c072b5af52d4c97a43b2f8395b080ce7c08f6103
jatin foujdar <jatin321001@gmail.com>
2022-11-28 17:19:22 +0530

    backend commit

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express")
+const userRoutes = require("./route/userRoutes")
+const app = express();
+const connectToDb = require("./config/db")
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+connectToDb();
+app.use("/", userRoutes);
+
+
+module.exports = app;
